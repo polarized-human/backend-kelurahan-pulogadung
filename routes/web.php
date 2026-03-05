b5412568f87fc6ebfcf9474878d622f153cb6809
Abdul <abdulhikmal81@gmail.com>
2026-03-05 14:24:36 +0700

    Add symlink route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/perbaiki-gambar', function () {
+    Artisan::call('storage:unlink'); // Hapus jembatan lama yang rusak (jika ada)
+    Artisan::call('storage:link');   // Buat jembatan baru yang segar
+    return 'Jembatan gambar berhasil diperbaiki!';
 });
