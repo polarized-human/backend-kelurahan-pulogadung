908aa2f38bfa21ac769edef53424e439cd42ceb9
Abdul <abdulhikmal81@gmail.com>
2026-03-12 14:20:06 +0700

    Memperbaiki create berita tidak langsung merujuk ke halaman edit

--- app/Filament/Resources/BeritaResource/Pages/CreateBerita.php
+++ app/Filament/Resources/BeritaResource/Pages/CreateBerita.php
@@ -9,4 +9,10 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateBerita extends CreateRecord
 {
     protected static string $resource = BeritaResource::class;
+
+    // Tambahkan blok kode di bawah ini
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
